8d0c97f2fc86c214fb8b8786434273beff9fc1ee
Panos Ioannidis <panos277@hotmail.com>
2019-05-05 02:03:13 +0300

    update dpcm encoder/decoder

--- exersice-6.17/dpcm_decoder.py
+++ exersice-6.17/dpcm_decoder.py
@@ -10,24 +10,34 @@ success, first_frame = video.read()
 first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
 
 # Set video codec
-video_codec = cv2.VideoWriter_fourcc(*'DIVX')
+video_codec = cv2.VideoWriter_fourcc(*'XVID')
 
 # Setup the output
 output = cv2.VideoWriter('decoded_video.mp4', video_codec, int(video.get(5)), (int(video.get(3)), int(video.get(4))), False)
+output.write(first_frame)
 
 frame_num = 0
 
 previous_frame = first_frame
 while video.isOpened():
+    # Skip the first frame because we load it outside the loop
+    if frame_num == 0:
+        success, current_frame = video.read()
+        frame_num += 1
+        continue
+
     success, current_frame = video.read()
     frame_num += 1
 
     if not success:
         break
 
+    # Frame to BW
     current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
 
-    current_frame = (current_frame + previous_frame) * 10
+    # Sum frames
+    current_frame = previous_frame +  current_frame
+
     previous_frame = current_frame
     output.write(current_frame)
 
--- exersice-6.17/dpcm_encoder.py
+++ exersice-6.17/dpcm_encoder.py
@@ -10,15 +10,22 @@ success, first_frame = video.read()
 first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
 
 # Set video codec
-video_codec = cv2.VideoWriter_fourcc(*'DIVX')
+video_codec = cv2.VideoWriter_fourcc(*'XVID')
 
 # Setup the output
 output = cv2.VideoWriter('encoded_video.mp4', video_codec, int(video.get(5)), (int(video.get(3)), int(video.get(4))), False)
+output.write(first_frame)
 
 frame_num = 0
 
 previous_frame = first_frame
 while video.isOpened():
+    # Skip the first frame because we load it outside the loop
+    if frame_num == 0:
+        success, current_frame = video.read()
+        frame_num += 1
+        continue
+
     success, current_frame = video.read()
     frame_num += 1
 
@@ -29,7 +36,7 @@ while video.isOpened():
     current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
 
     # Calculate difference and then quantize it
-    diff = (current_frame - previous_frame) // quantization_value
+    diff = (current_frame  - previous_frame) // quantization_value
 
     # Add result to output
     output.write(diff)
