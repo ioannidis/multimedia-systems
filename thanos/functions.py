f24d0584a9d12c52555667b58e49832d8ef83a03
Thanos Paravantis <thanosparavantis@gmail.com>
2019-05-31 12:26:48 +0300

    Add comments

--- thanos/functions.py
+++ thanos/functions.py
@@ -6,11 +6,13 @@ k = 16
 
 
 def frame_to_macroblocks(frame):
+    # Tweak the width and height of the frame so we can extract macroblocks of specific size.
     old_width = frame.shape[0]
     width = fit_size(old_width)
     old_height = frame.shape[1]
     height = fit_size(old_height)
 
+    # Find the padding to be applied on the frame.
     width_pad = (0, width - old_width)
     height_pad = (0, height - old_height)
     depth_pad = (0, 0)
@@ -18,6 +20,7 @@ def frame_to_macroblocks(frame):
 
     padded_frame = np.pad(frame, padding, mode='constant')
 
+    # Create 16x16 blocks based on the x-y plane of the frame.
     macroblocks = []
 
     for w in range(0, width - window, window):
@@ -27,30 +30,40 @@ def frame_to_macroblocks(frame):
             row.append(macroblock)
         macroblocks.append(row)
 
+    # We return rows of macroblocks so the shape is:
+    # (number of rows, number of macroblocks, 16, 16, 3)
     return macroblocks
 
 
 def macroblocks_to_frame(macroblocks):
+    # First we need to concatenate all macroblocks in the x plane for each row.
     rows = []
     for row in macroblocks:
         rows.append(np.concatenate([macroblock for macroblock in row], axis=1))
 
+    # Then we concatenate all rows of macroblocks in the y plane.
     frame = np.concatenate([row for row in rows], axis=0)
+
+    # Return the reconstructed frame that differs from the original one in shape.
     return frame
 
 
 def fit_size(x):
+    # Find the closest integer based on x that is divisible by 16.
     return window * ceil(x / window)
 
 
 def get_sad(m_prev, m_next):
     value = 0
 
+    # Loop through every pixel of each frame.
     for i in range(window):
         for j in range(window):
+            # Get the RGB info of both pixels.
             pixel_prev = m_prev[i, j]
             pixel_next = m_next[i, j]
 
+            # Find the absolute difference for all three color components.
             for k in range(3):
                 color_prev = int(pixel_prev[k])
                 color_next = int(pixel_next[k])
@@ -66,6 +79,7 @@ def get_best_match(ms_prev, next_row, next_col, m_next):
     result = None
 
     while step != 1:
+        # For each step find all 8 closest neighbours if the indexes are valid.
         neighbours = []
 
         try:
@@ -108,12 +122,14 @@ def get_best_match(ms_prev, next_row, next_col, m_next):
         except IndexError:
             pass
 
+        # Find a neighbouring macroblock with the best SAD value.
         sad_values = [get_sad(neighbour[2], m_next) for neighbour in neighbours]
         sad_min = min(sad_values)
 
         min_index = sad_values.index(sad_min)
         next_row, next_col, m_next = neighbours[min_index]
 
+        # Shift center and keep going until the step is 1.
         step /= 2
         result = m_next
 
--- thanos/object_removal.py
+++ thanos/object_removal.py
@@ -5,9 +5,7 @@ import numpy as np
 if __name__ == '__main__':
     video = cv2.VideoCapture('video1.mp4')
 
-    _, start_frame = video.read()
-
-    macroblocks_start = frame_to_macroblocks(start_frame)
+    macroblocks_prev = None
 
     while video.isOpened():
         _, frame = video.read()
@@ -16,29 +14,32 @@ if __name__ == '__main__':
             break
 
         macroblocks = frame_to_macroblocks(frame)
-        macroblocks_edited = macroblocks.copy()
+        macroblocks_original = macroblocks.copy()
 
-        macroblocks_edited[9] = macroblocks_start[9]
-        macroblocks_edited[10] = macroblocks_start[10]
-        macroblocks_edited[11] = macroblocks_start[11]
-        macroblocks_edited[12] = macroblocks_start[12]
-        macroblocks_edited[13] = macroblocks_start[13]
-        macroblocks_edited[14] = macroblocks_start[14]
-        macroblocks_edited[15] = macroblocks_start[15]
-        macroblocks_edited[16] = macroblocks_start[16]
-        macroblocks_edited[17] = macroblocks_start[17]
-        macroblocks_edited[18] = macroblocks_start[18]
-        macroblocks_edited[19] = macroblocks_start[19]
-        macroblocks_edited[20] = macroblocks_start[20]
-        macroblocks_edited[21] = macroblocks_start[21]
+        if macroblocks_prev is None:
+            macroblocks_prev = macroblocks
+            continue
 
-        before = macroblocks_to_frame(macroblocks)
-        after = macroblocks_to_frame(macroblocks_edited)
+        macroblocks[9] = macroblocks_prev[9]
+        macroblocks[10] = macroblocks_prev[10]
+        macroblocks[11] = macroblocks_prev[11]
+        macroblocks[12] = macroblocks_prev[12]
+        macroblocks[13] = macroblocks_prev[13]
+        macroblocks[14] = macroblocks_prev[14]
+        macroblocks[15] = macroblocks_prev[15]
+        macroblocks[16] = macroblocks_prev[16]
+        macroblocks[17] = macroblocks_prev[17]
+        macroblocks[18] = macroblocks_prev[18]
+        macroblocks[19] = macroblocks_prev[19]
+        macroblocks[20] = macroblocks_prev[20]
+        macroblocks[21] = macroblocks_prev[21]
 
+        before = macroblocks_to_frame(macroblocks_original)
+        after = macroblocks_to_frame(macroblocks)
         image = np.concatenate((before, after), axis=1)
 
+        macroblocks_prev = macroblocks
         cv2.imshow('Frame', image)
-
         cv2.waitKey(30)
 
     video.release()
