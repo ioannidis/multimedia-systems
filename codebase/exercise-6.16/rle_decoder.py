61c2bcf7a359d86c2fe6a73e5f09ff229a7e1ac2
Panos Ioannidis <panos277@hotmail.com>
2019-06-06 21:23:26 +0300

    Update 6.14

--- codebase/exercise-6.16/rle_decoder.py
+++ codebase/exercise-6.16/rle_decoder.py
@@ -5,30 +5,21 @@ image_string = ""
 with open('compressed_mona_lisa', 'r') as file:
     image_string = file.read().replace('\n', '')
 
-a = image_string.split("|")
-print(a)
-image_dimensions = a[0].split("x")
-quantization_value = a[1]
+image_data = image_string.split("|")
 
-encoded_image = a[2].split(",")
+image_dimensions = image_data[0].split("x")
+quantization_value = image_data[1]
 
-print(encoded_image)
+encoded_image = image_data[2].split(",")
 
-# Solution 3
 arr = []
 for node in encoded_image:
     data = node.split(";")
     arr += [data[1] for _ in range(int(data[0]))]
 
-print(arr)
-
 reverse_quantization_image = np.array(arr, dtype=int) * 10
 
-print(len(arr))
-
 image_as_2d_array = np.reshape(reverse_quantization_image, (int(image_dimensions[0]), int(image_dimensions[1])))
-
-print(image_as_2d_array)
 
 decoded_image = Image.fromarray(image_as_2d_array)
 decoded_image.show()
